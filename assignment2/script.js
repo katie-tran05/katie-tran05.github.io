8f055c01fdfee40379e3975ebdfc85f9e32633d4
katie-tran05 <s4026243@rmit.edu.vn>
2025-05-02 16:59:43 +1000

    update

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,21 +1,46 @@
-const video = document.querySelector("#custom-video-player");
-const playPauseBtn = document.querySelector("#play-pause-btn");
-const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
-function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
-  } else {
-    video.pause();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
-  }
-}
-function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
-}
+const videoList = [
+  {
+    video: 'icantfeelanything.mp4',
+    title: 'NCT DREAM 'icantfeelanything''
+  },
+  {
+    video: 'UNKNOWN.mp4',
+    title: 'NCT DREAM 'UNKNOWN''
+  },
+  {
+    video: 'BOX.mp4',
+    title: 'NCT DREAM 'BOX''
+  },
+  {
+    video: 'Breathing.mp4',
+    title: 'NCT DREAM 'Breathing''
+  },
+  {
+    video: 'Carat Cake.mp4',
+    title: 'NCT DREAM 'Carat Cake''
+  },
+]
+const categories = [...new Set(videoList.map((item)=> {return item}))]
+document.getElementById('videoList').innerHTML = categories.map((item)=> {
+  var{ video,title} = item;
+  return (
+    `<div class="list active">
+      <video src=${video} class="list-video"></video>
+      <h3 class="list-title">${title}</h3>
+    </div>`
+  )
+}).join('')
+let videoList = document.querySelectorAll('.video-list .list');
+videoList.forEach(remove =>{remove.classList.remove('active')});
+videoList.forEach(vid =>{
+  vid.onclick = ()=>{
+    videoList.forEach(remove=>{remove.classList.remove('active')});
+    vid.classList.add('active');
+    let src= vid.querySelector('.list-video').src;
+    let title = vid.querySelector('.list-title').innerHTML;
+    document.querySelector('.main-video-container .main-video').src = src;
+    document.querySelector('.main-video-container .main-video').play();
+    document.querySelector('.main-video-container .main-video-title').innerHTML = title;
+  };
+});
 // Add other functionalities here
